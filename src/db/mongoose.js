04d86b10795dca022187eda4418f7c7edda3ba36
Aviral Gangwar <aviralgangwar24@gmail.com>
2019-12-10 06:23:36 +0530

    Create Task model

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,33 +5,3 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useUnifiedTopology: true,
   useCreateIndex: true
 });
-
-const Tasks = mongoose.model("Tasks", {
-  description: {
-    type: String
-  },
-  completed: {
-    type: Boolean,
-    validate(value) {
-      if (value < 0) {
-        throw new Error("Age must be a positive number.");
-      }
-    }
-  }
-});
-
-// const task = new Tasks({
-//   description: "Cracking GSOC",
-//   completed: false
-// });
-
-// task
-//   .save()
-//   .then(() => {
-//     console.log(task);
-//   })
-//   .catch(error => {
-//     console.log("Error", error);
-//   });
-
-module.exports = mongoose;
