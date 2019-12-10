8eb8ef89072a05940ea9c73428b3ab5756819de7
Aviral Gangwar <aviralgangwar24@gmail.com>
2019-12-10 21:00:47 +0530

    Added token storage

--- src/models/user.js
+++ src/models/user.js
@@ -39,12 +39,22 @@ const userSchema = new mongoose.Schema({
         throw new Error("Age must be a positive number");
       }
     }
-  }
+  },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true
+      }
+    }
+  ]
 });
 
 userSchema.methods.generateAuthToken = async function() {
   const user = this;
   const token = jwt.sign({ _id: user._id.toString() }, "thisisataskapp");
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
   return token;
 };
 
