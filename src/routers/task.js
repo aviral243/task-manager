1e417046c2f1c16a29e2b256ba2992d8667805fc
Aviral Gangwar <aviralgangwar24@gmail.com>
2019-12-11 00:54:31 +0530

    Task routes refactored-2

--- src/routers/task.js
+++ src/routers/task.js
@@ -25,7 +25,7 @@ router.get("/tasks/:id", auth, async (req, res) => {
   }
 });
 
-router.patch("/tasks/:id", async (req, res) => {
+router.patch("/tasks/:id", auth, async (req, res) => {
   const updates = Object.keys(req.body);
   const allowedUpdates = ["description", "completed"];
   const isValidOperation = updates.every(update => {
@@ -35,11 +35,10 @@ router.patch("/tasks/:id", async (req, res) => {
     return res.status(400).send({ error: "Invalid updates!" });
   }
   try {
-    // const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
-    // new: true,
-    //   runValidators: true
-    // });
-    const task = await Task.findById(req.params.id);
+    const task = await Task.findOne({
+      _id: req.params.id,
+      owner: req.user._id
+    });
     updates.forEach(update => {
       task[update] = req.body[update];
     });
