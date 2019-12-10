042d935ea5c0d999ea20898792d13226a0181664
Aviral Gangwar <aviralgangwar24@gmail.com>
2019-12-10 21:56:49 +0530

    Changed /users route

--- src/routers/user.js
+++ src/routers/user.js
@@ -27,14 +27,10 @@ router.post("/users/login", async (req, res) => {
   }
 });
 
-router.get("/users", auth, async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (e) {
-    res.status(500).send(e);
-  }
+router.get("/users/me", auth, async (req, res) => {
+  res.send(req.user);
 });
+
 router.get("/users/:id", async (req, res) => {
   const _id = req.params.id;
 
