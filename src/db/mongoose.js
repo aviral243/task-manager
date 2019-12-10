072e42a344d27fb8f7de100109c3b6ed8ed117f1
Aviral Gangwar <aviralgangwar24@gmail.com>
2019-12-10 06:12:51 +0530

    Created User model

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,33 +1,10 @@
 const mongoose = require("mongoose");
-const validator = require("validator");
 
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true
 });
-
-// const User = mongoose.model("User", {
-//   name: {
-//     type: String
-//   },
-//   age: {
-//     type: Number
-//   }
-// });
-
-// const me = new User({
-//   name: "Aviral",
-//   age: 20
-// });
-
-// me.save()
-//   .then(() => {
-//     console.log(me);
-//   })
-//   .catch(error => {
-//     console.log("Error", error);
-//   });
 
 const Tasks = mongoose.model("Tasks", {
   description: {
@@ -43,16 +20,18 @@ const Tasks = mongoose.model("Tasks", {
   }
 });
 
-const task = new Tasks({
-  description: "Cracking GSOC",
-  completed: false
-});
+// const task = new Tasks({
+//   description: "Cracking GSOC",
+//   completed: false
+// });
 
-task
-  .save()
-  .then(() => {
-    console.log(task);
-  })
-  .catch(error => {
-    console.log("Error", error);
-  });
+// task
+//   .save()
+//   .then(() => {
+//     console.log(task);
+//   })
+//   .catch(error => {
+//     console.log("Error", error);
+//   });
+
+module.exports = mongoose;
